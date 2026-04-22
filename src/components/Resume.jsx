103ae532a625443660dc982f4772dedb8fe92972
LacsJP27 <jplacsamana@gmail.com>
2026-04-22 13:01:09 -0500

    put resume into public folder

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
         <div style={{ position: "fixed", inset: 0, width: "100vw", height: "100vh" }}>
             <iframe
-                src="/JPLacsamana_Resume.pdf"
+                src="/Joseph_Lacsamana_Resume.pdf"
                 title="Resume"
                 width="100%"
                 height="100%"
